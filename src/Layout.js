60460a742834d98ee8003323ed1a08bd8b08960c
Quang Nguyen <quangnguyenmail96@gmail.com>
2022-03-10 12:02:53 +0700

    fix minor bugs

--- src/Layout.js
+++ src/Layout.js
@@ -1,27 +1,18 @@
 import { Layout, Menu } from "antd"
-import React, { useEffect } from "react"
-import { useMatch } from "react-router"
+import React from "react"
 import { Link } from "react-router-dom"
 import logo from "./asset/Images/logoiBenefit.png"
 import "./App.css"
 
 const { Sider, Header, Content } = Layout
 export default function MyLayout({ children }) {
-  const routeMatch = useMatch("/question2")
-  useEffect(() => {
-    console.log("routeMatch", routeMatch)
-  }, [])
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Sider style={{ height: "100vh" }} theme="light">
         <div className="layout-logo">
           <img width={"100%"} height={"100%"} src={logo} alt="logo" />
         </div>
-        <Menu
-          defaultSelectedKeys={["/question1"]}
-          selectedKeys={routeMatch?.pathname}
-          mode="inline"
-        >
+        <Menu defaultSelectedKeys={["/question1"]} mode="inline">
           <Menu.Item key={"/question1"}>
             <Link to="/question1">Question 1</Link>
           </Menu.Item>
